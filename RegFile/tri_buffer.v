fb3930d378dabc9a969d1b54de4afff060ebc3e0
kaifeng-yu16 <90477174+kaifeng-yu16@users.noreply.github.com>
2021-10-07 17:09:06 -0400

    Delete tri_buffer.v

--- RegFile/tri_buffer.v
+++
@@ -1,7 +0,0 @@
-module tri_buffer(buffer_out, buffer_in, select);
-	output [31:0] buffer_out;
-	input select;
-	input [31:0] buffer_in;
-	
-	assign buffer_out = select ? buffer_in : 32'bzzzz_zzzz_zzzz_zzzz_zzzz_zzzz_zzzz_zzzz;
-endmodule 
